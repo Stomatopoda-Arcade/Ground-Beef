03823269ab0433490cf5a43580055c8e8a7d47de
Paul Harrison <pgharrison25@gmail.com>
2024-07-28 11:05:46 -0400

    created cow object

---
+++ cow.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+var cow_flipped = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	cow_flipped = !cow_flipped
+	$Sprite2D.flip_h = cow_flipped
+	pass # Replace with function body.
