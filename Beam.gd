8cbe92c5b991b7d5cfc294ee1d9a1279e82f3557
Paul Harrison <pgharrison25@gmail.com>
2024-07-28 17:40:15 -0400

    added event handlers for beam objects

--- Beam.gd
+++ Beam.gd
@@ -1,14 +1,17 @@
 extends Area2D
 
 enum BEAM_TYPE {OFF,TRACTOR,DEATH}
+
 var beam_locked = false
 var active_beam = BEAM_TYPE.OFF
+
+signal object_abducted(groups)
+signal object_destroyed(groups)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -35,13 +38,19 @@ func set_beam(type):
 		$DeathBeam.visible = false
 		$TractorBeam.visible = false
 		$CollisionShape2D.disabled = true
-	
-
 
 func _on_display_timer_timeout():
 	set_beam(BEAM_TYPE.OFF)
 	$RechargeTimer.start()
 
-
 func _on_recharge_timer_timeout():
 	beam_locked = false
+
+
+func _on_body_entered(body):
+	if body.is_in_group("abductable") and active_beam == BEAM_TYPE.TRACTOR:
+		object_abducted.emit(body.get_groups())
+		body.queue_free()
+	elif body.is_in_group("destroyable") and  active_beam == BEAM_TYPE.DEATH:
+		object_destroyed.emit(body.get_groups())
+		body.queue_free()
--- cow.gd
+++ cow.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+class_name Cow
+
 var cow_flipped = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
