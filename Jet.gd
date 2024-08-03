77876309b98de7047189a79d1b702f8411b5f80d
Paul Harrison <pgharrison25@gmail.com>
2024-08-03 18:08:30 -0400

    created jet object

---
+++ Jet.gd
@@ -0,0 +1,43 @@
+extends StaticBody2D
+const ProjectileResource = preload("res://projectile.tscn")
+enum JET_FACING {JET_LEFT,JET_RIGHT}
+enum DIFFICULTY {EASY,MEDIUM,HARD}
+
+@export var projectile_velocity = 250 
+@export var jet_velocity = 50
+@export var starting_facing = JET_FACING.JET_RIGHT
+@export var starting_difficulty = DIFFICULTY.EASY
+var projectile_angle = 0
+var direction = Vector2(1,0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if starting_facing == JET_FACING.JET_RIGHT:
+		direction = Vector2(1,0)
+		projectile_angle = 0
+		$JetSprite.flip_h = false
+	elif starting_facing == JET_FACING.JET_LEFT:
+		direction = Vector2(-1,0)
+		projectile_angle = 180
+		$JetSprite.flip_h = true
+
+	if starting_difficulty == DIFFICULTY.EASY:
+		$Timer.autostart = false
+	elif starting_difficulty == DIFFICULTY.MEDIUM:
+		pass
+	elif starting_difficulty == DIFFICULTY.HARD:
+		$Timer.wait_time = 2
+		$Timer.autostart = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += direction*jet_velocity*delta
+
+
+func _on_timer_timeout():
+	var projectile_instance = ProjectileResource.instantiate()
+	projectile_instance.velocity = projectile_velocity
+	projectile_instance.angle = projectile_angle
+	self.add_child(projectile_instance)
+	self.move_child(projectile_instance,0)
